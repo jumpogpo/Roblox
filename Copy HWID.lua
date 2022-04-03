b10a405c2e3caeacc85fe3f314f85988c008e561
Thanapat Koedpiam <chokhun_@hotmail.com>
2022-04-03 23:31:09 +0700

    Update Copy HWID.lua

--- Copy HWID.lua
+++ Copy HWID.lua
@@ -1,33 +1,39 @@
+--[[
+ __      _______ _____  ______ _____  
+ \ \    / /_   _|  __ \|  ____|  __ \ 
+  \ \  / /  | | | |__) | |__  | |__) |
+   \ \/ /   | | |  ___/|  __| |  _  / 
+    \  /   _| |_| |    | |____| | \ \ 
+     \/   |_____|_|    |______|_|  \_\
+
+]]--
+
+
 local http_request = http_request;
+
 if syn then
-http_request = syn.request
-elseif SENTINEL_V2 then
-function http_request(tb)
-return {
-StatusCode = 200;
-Body = request(tb.Url, tb.Method, (tb.Body or ''))
-}
-end
+	http_request = syn.request
 end
 
 local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
 local decoded = game:GetService('HttpService'):JSONDecode(body)
-local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint","Krnl-Fingerprint"};
+local hwid_list = {"Syn-Fingerprint", "Krnl-Fingerprint"};
 local hwid = "";
 
 for i, v in next, hwid_list do
-if decoded.headers[v] then
-hwid = decoded.headers[v];
-break
-end
+	if decoded.headers[v] then
+		hwid = decoded.headers[v];
+		break
+	end
 end
 
 if hwid then
-setclipboard(hwid)
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "EZ BY UNIQUE ";
-    Text = "SUCCESS COPIED HWID";
-})
+	setclipboard(hwid)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+       	Title = "Get HWID Success";
+        Text = "Copy HWID Already\nPress Ctrl + V to paste";
+        Duration = 9999;
+    })
 else
-game:GetService("Players").LocalPlayer:Kick('unable to find hwid')
+	game:GetService("Players").LocalPlayer:Kick('unable to find hwid')
 end
