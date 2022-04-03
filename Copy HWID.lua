dfa173d26141733fbdef46e3f3caf51ffb95383d
Thanapat Koedpiam <chokhun_@hotmail.com>
2022-04-03 23:09:40 +0700

    Update Copy HWID.lua

--- Copy HWID.lua
+++ Copy HWID.lua
@@ -26,7 +26,7 @@ end
 
 local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
 local decoded = game:GetService('HttpService'):JSONDecode(body)
-local hwid_list = {"Syn-Fingerprint", "KRNL"};
+local hwid_list = {"Syn-Fingerprint", "Krnl"};
 local hwid = "";
 
 for i, v in next, hwid_list do
